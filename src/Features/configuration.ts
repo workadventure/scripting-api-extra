5bb55b09183fb50400556a44ce20fbefeaf6d32e
Valdo <vteixeiraromao@gmail.com>
2021-10-12 17:29:11 +0100

    Local configuration (#64)
    feat: Added local configuration panel
    
    The configuration panel can now be filtered on some variables.
    Added a new function to open the configuration panel and special properties to open it too.

--- src/Features/configuration.ts
+++ src/Features/configuration.ts
@@ -1,23 +1,100 @@
 import { Properties } from "../Properties";
 import type { ITiledMap } from "@workadventure/tiled-map-type-guard/dist";
 import { defaultAssetsUrl } from "./default_assets_url";
+import { getLayersMap } from "../LayersFlattener";
+import type { ITiledMapLayer } from "@workadventure/tiled-map-type-guard/dist";
+import type { ActionMessage } from "@workadventure/iframe-api-typings/Api/iframe/Ui/ActionMessage";
+
+let layersMap!: Map<string, ITiledMapLayer>;
 
 /**
  * Initialize the configuration button in the menu
  */
 export async function initConfiguration(assetsUrl?: string | undefined): Promise<void> {
     const map: ITiledMap = await WA.room.getTiledMap();
-
+    assetsUrl = assetsUrl ?? defaultAssetsUrl;
+    layersMap = await getLayersMap();
     const configurationLayer = map.layers.find((layer) => layer.name === "configuration");
 
     if (configurationLayer) {
+        // Controls the configuration panel triggered from the menu
         const properties = new Properties(configurationLayer.properties);
         const tag = properties.getString("tag");
         if (!tag || WA.player.tags.includes(tag)) {
             WA.ui.registerMenuCommand("Configure the room", () => {
-                assetsUrl = assetsUrl ?? defaultAssetsUrl;
                 WA.nav.openCoWebSite(assetsUrl + "/configuration.html", true);
             });
         }
+
+        // Controls the configuration panel triggered from a zone
+        for (const layer of layersMap.values()) {
+            const properties = new Properties(layer.properties);
+            const openConfigVariables = properties.getString("openConfig");
+            if (openConfigVariables && layer.type === "tilelayer") {
+                initConfigurationPanel(openConfigVariables, properties);
+            }
+        }
     }
+}
+
+function initConfigurationPanel(openConfigVariables: string, properties: Properties): void {
+    let actionMessage: ActionMessage | undefined = undefined;
+
+    const zoneName = properties.getString("zone");
+    if (!zoneName) {
+        throw new Error('Missing "zone" property');
+    }
+
+    const tag = properties.getString("openConfigAdminTag");
+    let allowedByTag = true;
+    if (tag && !WA.player.tags.includes(tag)) {
+        allowedByTag = false;
+    }
+
+    function displayConfigurationMessage(): void {
+        if (actionMessage) {
+            actionMessage.remove();
+        }
+        actionMessage = WA.ui.displayActionMessage({
+            message:
+                properties.getString("openConfigTriggerMessage") ??
+                "Press SPACE or touch here to configure",
+            callback: () => openConfigurationPanel(openConfigVariables),
+        });
+    }
+
+    function closeConfigurationPanel(): void {
+        WA.nav.closeCoWebSite();
+    }
+
+    WA.room.onEnterZone(zoneName, () => {
+        const openConfigTriggerValue = properties.getString("openConfigTrigger");
+
+        // Do not display conf panel if the user is not allowed by tag
+        if (allowedByTag) {
+            if (openConfigTriggerValue && openConfigTriggerValue === "onaction") {
+                displayConfigurationMessage();
+            } else {
+                openConfigurationPanel(openConfigVariables);
+            }
+        }
+    });
+
+    WA.room.onLeaveZone(zoneName, () => {
+        if (actionMessage) {
+            actionMessage.remove();
+            closeConfigurationPanel();
+        } else {
+            closeConfigurationPanel();
+        }
+    });
+}
+
+/**
+ * Open the configuration panel inside a iframe and filter which variables will be displayed
+ * @param variablesToConfigure Variable names separated by a comma, e.g.: var1,var2
+ */
+function openConfigurationPanel(variablesToConfigure?: string): void {
+    const parameters = variablesToConfigure ? "#" + variablesToConfigure : "";
+    WA.nav.openCoWebSite(defaultAssetsUrl + "/configuration.html" + parameters, true);
 }
--- src/Iframes/Configuration/Components/Section.svelte
+++ src/Iframes/Configuration/Components/Section.svelte
@@ -5,38 +5,52 @@
     import {VariableDescriptor} from "../../../VariablesExtra";
     import Field from "./Field.svelte";
     import {currentPage} from "../Stores/currentPage";
-
-    //export let WA: WorkAdventureApi;
     export let layer: ITiledMapObjectLayer;
 
     const properties = new Properties(layer.properties);
 
-    const variables = layer.objects.filter((object) => object.type === 'variable').map((variable) => new VariableDescriptor(variable));
+    // remove the "#" in order to split the hash into an array
+    let hash = window.location.hash
+    hash = hash.substring(1);
+    const variablesToDisplay = hash.split(',');
 
+    let variables = initVariables()
+    // if a hash is passed in the URL, use the parameters to filter the variables to display
+    if (hash) variables = initFilterVariables()
+
+    function initVariables() {
+        return layer.objects.filter((object) => object.type === 'variable').map((variable) => new VariableDescriptor(variable));
+    }
+    function initFilterVariables() {
+        return layer.objects.filter((object) => object.type === 'variable' && variablesToDisplay.includes(object.name)).map((variable) => new VariableDescriptor(variable));
+    }
 </script>
 
-<div>
-        <div class="{ properties.get('name') ? 'nes-container with-title': '' }">
-            {#if properties.get("name") }
-                <p class="title">{ properties.get("name") }</p>
-            {/if}
-
-            {#each variables as variable }
-                {#if variable.isReadable }
-                    <Field variable={variable} />
-                {/if}
-            {/each}
-        </div>
-</div>
-
 {#if $currentPage !== 'configuration' }
-<div class="center">
-    <button class="nes-btn" on:click={() => { $currentPage = $currentPage.substr(0, $currentPage.indexOf('/')) }}>&lt; Back</button>
-</div>
+    <div class="left mb-20">
+        <button class="nes-btn" on:click={() => { $currentPage = $currentPage.substr(0, $currentPage.indexOf('/')) }}>&lt; Back</button>
+    </div>
 {/if}
 
+<div>
+    <div class="{ properties.get('name') ? 'nes-container with-title': '' }">
+            {#if properties.get("name") }
+            <p class="title">{ properties.get("name") }</p>
+        {/if}
+
+        {#each variables as variable }
+            {#if variable.isReadable}
+                <Field variable={variable} />
+            {/if}
+        {/each}
+    </div>
+</div>
+
 <style lang="scss">
-    div.center {
-        text-align: center;
+    div.left {
+        text-align: left;
+    }
+    .mb-20 {
+        margin-bottom: 20px;
     }
 </style>
