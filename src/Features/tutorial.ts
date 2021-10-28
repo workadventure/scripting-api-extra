e13bcc5ee0d11f1370ab7c94f8db4fad95b40e81
Benedicte Quimbert <b.quimbert@workadventu.re>
2021-10-28 16:19:31 +0200

    WIP no overflows on sides

--- src/Features/tutorial.ts
+++ src/Features/tutorial.ts
@@ -1,10 +1,13 @@
 export function initTutorial(): void {
     WA.onInit().then(() => {
+        //@ts-ignore
         const tutorialDone = WA.player.state.tutorialDone;
         if (!tutorialDone) {
-            WA.player.getStartPosition().then((position: Position) => {
+            //@ts-ignore
+            WA.player.getPosition().then((position: Position) => {
                 openTutorial(position);
-                WA.player.state.tutorialDone = true;
+                //@ts-ignore
+                //WA.player.state.tutorialDone = true;
             });
         }
     });
@@ -15,6 +18,7 @@ export function openTutorial(position: Position): void {
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         // Creates tutorial iFrame for mobile devices
         const frameWidth = 375;
+        const frameHeight = 600;
         WA.room.website.create({
             name: "tutorial",
             url: "/tutorial.html",
@@ -28,19 +32,49 @@ export function openTutorial(position: Position): void {
             allowApi: true,
         });
     } else {
-        // Create tutorial iFrame for web desktop
-        const frameWidth = 600;
-        WA.room.website.create({
-            name: "tutorial",
-            url: "/tutorial.html",
-            position: {
-                x: position.x - frameWidth / 2,
-                y: position.y + 96,
-                width: frameWidth,
-                height: screen.height,
-            },
-            visible: true,
-            allowApi: true,
+        //TODO: place it elsewhere
+        WA.room.getTiledMap().then((currentMap) => {
+            console.log(currentMap);
+            // Setup for iframe coordinates calculations
+            const frameWidth: number = 600;
+            const frameWidthAsTiles: number = frameWidth / currentMap.tilewidth;
+
+            const frameHeight: number = 450;
+            const frameHeightAsTiles: number = frameHeight / currentMap.tileheight;
+
+            const margin: number = 10;
+
+            let frameLeft = position.x - frameWidth / 2;
+            let frameTop: number = position.y + currentMap.tileheight;
+            let frameRight: number = frameLeft + frameWidth;
+            let frameBottom: number = frameTop + frameHeight;
+
+            //Correct starting x position if the iFrame crosses the map's left limit
+            if (frameLeft < 0) {
+                frameLeft = margin;
+            }
+
+            //Correct ending x position if the iFram crosses the map's right limit
+            console.log("flag mapwidth px", currentMap.width * currentMap.tilewidth);
+            console.log("flag frame rightx", frameRight);
+            if (frameRight > currentMap.width * currentMap.tilewidth) {
+                const overflow = frameRight - currentMap.width * currentMap.tilewidth;
+                frameLeft = frameLeft - overflow - margin;
+            }
+
+            // Create tutorial iFrame for web desktop
+            WA.room.website.create({
+                name: "tutorial",
+                url: "/tutorial.html",
+                position: {
+                    x: frameLeft,
+                    y: frameTop,
+                    width: frameWidth,
+                    height: frameHeight,
+                },
+                visible: true,
+                allowApi: true,
+            });
         });
     }
 }
@@ -48,7 +82,8 @@ export function openTutorial(position: Position): void {
 export function replay(): void {
     WA.room.website.delete("tutorial");
     //TODO: use CURRENT position of the player
-    WA.player.getStartPosition().then((position: Position) => {
+    //@ts-ignore
+    WA.player.getPosition().then((position) => {
         openTutorial(position);
     });
 }
