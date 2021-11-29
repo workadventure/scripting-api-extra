295f77a9d67e7929259a53bea6f3abd27d693b90
Benedicte Quimbert <b.quimbert@workadventu.re>
2021-11-29 17:00:07 +0100

    horizontal borders based on available area

--- src/Features/tutorial.ts
+++ src/Features/tutorial.ts
@@ -1,3 +1,6 @@
+import type { HasPlayerMovedEvent } from "@workadventure/iframe-api-typings/Api/Events/HasPlayerMovedEvent";
+import type { WasCameraUpdatedEvent } from "../../../workadventure/front/src/Api/Events/WasCameraUpdatedEvent";
+
 export async function initTutorial(): Promise<void> {
     //const tutorialDone = WA.player.state.tutorialDone;
     const tutorialDone = false; //TODO: delete and uncomment
@@ -6,48 +9,59 @@ export async function initTutorial(): Promise<void> {
         openTutorial();
 
         //On camera (worldView) movement, we want to make sure that the tutorial stays visible
-        let lastWorldView = {};
+        let playerPosition: HasPlayerMovedEvent = await WA.player.getPosition();
+        let camera: WasCameraUpdatedEvent;
 
-        //TODO: fix type
-        WA.camera.onCameraMove(async (worldView: any) => {
-            if (worldView == lastWorldView) {
+        const tutorialIFrame = await WA.room.website.get("tutorial");
+
+        const updateTutorialPosition = (): void => {
+            if (camera === undefined) {
                 return;
             }
 
-            const playerPosition = await WA.player.getPosition();
-            const tutorialIFrame = await WA.room.website.get("tutorial");
+            const margin = 16;
 
-            //Tutorial iFrame borders
-            const tutorialTop = playerPosition.y + tutorialIFrame.y;
-            const tutorialLeft = playerPosition.x + tutorialIFrame.x;
-            const tutorialRight = tutorialLeft + tutorialIFrame.width * tutorialIFrame.scale;
-            const tutorialBottom = tutorialTop + tutorialIFrame.height * tutorialIFrame.scale;
+            const cameraRight = camera.x + camera.width;
+            const cameraBottom = camera.y + camera.height;
 
-            //Correcting x position if the iFrame crosses the worldView's left limit
-            if (tutorialLeft < worldView.x) {
-                const overflow = worldView.x - tutorialLeft;
-                tutorialIFrame.x = tutorialIFrame.x + overflow;
+            const playerRight = playerPosition.x + margin;
+            const playerLeft = playerPosition.x - margin;
+            const playerBottom = playerPosition.y + margin;
+            const playerTop = playerPosition.y - margin;
+
+            //Correcting x position if the iframe crosses the horizontal borders
+            console.log("FLAG WV : ", camera);
+            console.log("FLAG PLAYER: ", playerPosition);
+            if (tutorialIFrame.width > cameraRight - playerRight) {
+                tutorialIFrame.x = -tutorialIFrame.width - 2 * margin;
+            } else if (tutorialIFrame.width > camera.x - playerLeft) {
+                console.log(
+                    `tutorial width: ${tutorialIFrame.width} | camera.x : ${camera.x} | player Left : ${playerLeft}`,
+                );
+                tutorialIFrame.x = 2 * margin;
             }
+            //
+            // //Correcting y position if the iframe crosses the vertical borders
+            // if (tutorialIFrame.height > cameraBottom - playerBottom) {
+            //     console.log("FLAG WV - Bottom: ", camera);
+            //     console.log("FLAG PLAYER - Bottom: ", playerPosition);
+            //     tutorialIFrame.y = -tutorialIFrame.height + 3 * margin;
+            // }
+            // if (tutorialIFrame.height > camera.y - playerTop) {
+            //     console.log("FLAG WV - Top: ", camera);
+            //     console.log("FLAG PLAYER - Top: ", playerPosition);
+            //     tutorialIFrame.y = 0;
+            // }
+        };
 
-            //Correcting x position if the iFrame crosses the worldView's right limit
-            const rightLimit = worldView.x + worldView.width;
-            if (tutorialRight > rightLimit) {
-                const overflow = tutorialRight - rightLimit;
-                tutorialIFrame.x = tutorialIFrame.x - overflow;
-            }
+        WA.player.onPlayerMove((position) => {
+            playerPosition = position;
+            updateTutorialPosition();
+        });
 
-            //Correcting y position if the iFrame crosses the worldView's top limit
-            if (tutorialTop < worldView.y) {
-                const overflow = worldView.y - tutorialTop;
-                tutorialIFrame.y = tutorialIFrame.y + overflow;
-            }
-
-            //Correcting y position if the iFrame crosses the worldView's bottom limit
-            const bottomLimit = worldView.y + worldView.height;
-            if (tutorialBottom > bottomLimit) {
-                const overflow = tutorialBottom - bottomLimit;
-                tutorialIFrame.y = tutorialIFrame.y - overflow;
-            }
+        WA.camera.onCameraUpdate(async (cameraPosition: any) => {
+            camera = cameraPosition;
+            updateTutorialPosition();
         });
 
         WA.player.state.tutorialDone = true;
@@ -55,37 +69,27 @@ export async function initTutorial(): Promise<void> {
 }
 
 export function openTutorial(): void {
+    const testWidth = 430;
+    const testHeight = 370;
+
+    let config = {
+        allow: "",
+        name: "tutorial",
+        url: "/tutorial.html",
+        position: {
+            height: testHeight,
+            width: testWidth,
+            x: 10,
+            y: -160,
+        },
+        visible: true,
+        allowApi: true,
+        origin: "player",
+        scale: 0.7,
+    };
+
     if (/Mobi|Android/i.test(navigator.userAgent)) {
-        WA.room.website.create({
-            allow: "",
-            name: "tutorial",
-            url: "/tutorial.html",
-            position: {
-                x: 150,
-                y: -40,
-                height: 700,
-                width: 375,
-            },
-            visible: true,
-            allowApi: true,
-            origin: "player",
-            scale: 0.7,
-        });
-    } else {
-        WA.room.website.create({
-            allow: "",
-            name: "tutorial",
-            url: "/tutorial.html",
-            position: {
-                x: 200,
-                y: -32,
-                height: 470,
-                width: 600,
-            },
-            visible: true,
-            allowApi: true,
-            origin: "player",
-            scale: 0.5,
-        });
+        config = { ...config, position: { x: 150, y: -40, height: 700, width: 375 }, scale: 0.7 };
     }
+    WA.room.website.create(config);
 }
