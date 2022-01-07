ff749c85afb7b7939500a64cbf6ee06941af88b7
Bénédicte Q <37311765+HimeShaman@users.noreply.github.com>
2022-01-07 15:40:38 +0100

    feat(tutorial): adding an easy to add "tutorial"
    Onboarding tutorial on first connection (#84)
    
    
    * First commit - adds whole feature
    
    * Removes ts-ignore annotations
    
    * Uses player state in order to get and set "tutorialDone" value
    
    * Prettier refactoring
    
    * Makes tutorial iframe open under the player
    
    * WIP no overflows on sides
    
    * No more overflowing
    
    * Refactoring and test map update
    
    * Makes dialog box slightly transparent so the player position is never fully hidden to the user
    
    * Typo fix
    
    * Deletes redo button + slight refactoring
    
    * WIP tutorial iteration
    
    * EndWIP: makes tutorial follow player and switches Walter for Tutopi
    
    * Adjusts html/css for mobile version
    
    * Uses the camera API to prevent the iframe from crossing worldView boundaries
    
    * horizontal borders based on available area
    
    * Mobile integration
    
    * Responsive style for both mobile and desktop
    
    * Adds new assets and make the iFrame fit whatever the scaling is
    
    * Adds new video assets and clean old ones + config adjusted
    
    * Uses the player state to trigger tutorial
    
    * Removes unused css
    
    * Test map updates: shortcut to corners
    
    * fix wrong original y value for the iframe
    
    * Completes the test map
    
    * Factorise regex for user agent
    
    * Removes unused console.log
    
    * Makes the tutorial optional depending on associated map prop
    
    * Adds documentation
    
    * Typo correction
    
    * Update docs/reference.md
    
    Co-authored-by: David Négrier <d.negrier@thecodingmachine.com>
    
    * imports WasCameraUpdatedEvent from typings package
    
    * Updates packages
    
    * Corrects types
    
    Co-authored-by: Benedicte Quimbert <b.quimbert@workadventu.re>
    Co-authored-by: David Négrier <d.negrier@thecodingmachine.com>

---
+++ docs/tutorial.md
@@ -0,0 +1,21 @@
+{.section-title.accent.text-primary}
+# Tutorial
+
+{.alert.alert-info}
+**Important!** To use the "tutorial" feature, you need to [import the "Scripting API Extra" script in your map](about.md#importing-the-extended-features)
+
+The tutorial displays a few messages in order to teach players how to use WorkAdventure's basic features. It uses the  [player variables](https://workadventu.re/map-building/api-player.md#player-specific-variables) in order to determine if it has not been seen yet and displays the iframe if necessary (if it has not been seen yet). 
+
+<figure class="figure">
+    <img class="figure-img img-fluid rounded" src="images/tutorial_preview.png" alt="" />
+    <figcaption class="figure-caption">The tutorial on desktop web browser</figcaption>
+</figure>
+
+To activate it, you only need to add the following property to your map:
+- `tutorial`: a boolean that indicates whether the map needs to display the tutorial or not. It should be checked if you wish to have a tutorial.
+<figure class="figure">
+    <img class="figure-img img-fluid rounded" src="images/tutorial_property.png" alt="" />
+    <figcaption class="figure-caption">How to get the tutorial for your maps</figcaption>
+</figure>
+
+
---
+++ src/Iframes/Tutorial/index.ts
@@ -0,0 +1,56 @@
+import "./style/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let currentStep: number | "end" = 1;
+    let firstMovementDone = false;
+
+    //Showing content according to the device
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        showContentForDevice("mobile");
+    } else {
+        showContentForDevice("desktop");
+    }
+
+    //Initializing all listened to buttons
+    initializeClickableBtns("next-btn", () => goToNextStep());
+
+    initializeClickableBtns("skip-btn", () => {
+        currentStep = "end";
+        WA.room.website.delete("tutorial");
+    });
+
+    //STEP 1 - does not use a button in order to be completed
+    WA.player.onPlayerMove(() => {
+        if (!firstMovementDone && typeof currentStep == "number") {
+            firstMovementDone = true;
+            goToNextStep();
+        }
+    });
+
+    // END STEP 1
+
+    function goToNextStep(): void {
+        if (currentStep === "end") {
+            return;
+        }
+        document.getElementById(`step-${currentStep}`)?.classList.add("hidden");
+        currentStep++;
+        document.getElementById(`step-${currentStep}`)?.classList.remove("hidden");
+    }
+
+    function showContentForDevice(device: string): void {
+        const contentList = document.getElementsByClassName(device);
+        for (const content of contentList) {
+            content.classList.remove("hidden");
+        }
+    }
+
+    function initializeClickableBtns(btnType: string, action: () => void): void {
+        const buttons = document.getElementsByClassName(btnType);
+        for (const btn of buttons) {
+            btn.addEventListener("click", () => {
+                action();
+            });
+        }
+    }
+});
