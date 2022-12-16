95c0a202a9e31dab56687baf7f01daa14e65a9a1
MIL <m.lima@workadventu.re>
2022-12-16 15:03:02 +0100

    Add adaptIframe and getId function

---
+++ src/Features/AdaptIframe.ts
@@ -0,0 +1,23 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+import type { UIWebsite } from "@workadventure/iframe-api-typings";
+
+export async function getIdByUrl(url: string) {
+    const website: UIWebsite = (await WA.ui.website.getAll()).find((obj) => obj.url.includes(url))!;
+
+    return website.id;
+}
+
+export async function adaptIframe(url: string, width: string, height: string): Promise<UIWebsite> {
+    return WA.onInit().then(async () => {
+        const website: UIWebsite = (await WA.ui.website.getAll()).find((obj) =>
+            obj.url.includes(url),
+        )!;
+        console.log({ website });
+        if (website) {
+            website.size.width = width;
+            website.size.height = height;
+        }
+        return website;
+    });
+}
