340aa79d4bf0b771d26d7d11c2d99331701231b8
MIL <m.lima@workadventu.re>
2023-01-25 10:26:46 +0100

    Add adaptIframe function

--- src/Features/AdaptIframe.ts
+++ src/Features/AdaptIframe.ts
@@ -1,21 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
-
 import type { UIWebsite } from "@workadventure/iframe-api-typings";
 
-export async function getIdByUrl(url: string) {
-    WA.onInit().then(async () => {
-        const website: UIWebsite = (await WA.ui.website.getAll()).find((obj) => obj.url.includes(url))!;
-
-        return website.id;
-    });
-}
-
-export async function adaptIframe(url: string, width: string, height: string): Promise<UIWebsite> {
+export async function adaptIframe(id: string, width: string, height: string): Promise<UIWebsite> {
     return WA.onInit().then(async () => {
-        const website: UIWebsite = (await WA.ui.website.getAll()).find((obj) =>
-            obj.url.includes(url),
-        )!;
-        console.log({ website });
+        const website: UIWebsite = WA.ui.website.getById(id);
         if (website) {
             website.size.width = width;
             website.size.height = height;
