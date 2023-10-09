e8ed486a4890006b1c353a23c2ba60994a4082ec
Gregoire Parant <gregoire@workadventu.re>
2023-10-09 14:13:39 +0200

    Refactor

--- src/Iframes/TutorialV1/Tuto/Components/Step.svelte
+++ src/Iframes/TutorialV1/Tuto/Components/Step.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     export let title: string;
-    export let shortTiltle: string;
+    export let shortTitle: string;
     export let videoUrl: string;
     export let videoPoster: string;
     export let description: string;
@@ -8,7 +8,7 @@
 <div class="ste tw-px-16 tw-py-5 tw-overflow-y-scroll">
     <div class="title tw-text-white">
         <h1 class="tw-p-3">{title}</h1>
-        <p class="tw-p-3">{shortTiltle}</p>
+        <p class="tw-p-3">{shortTitle}</p>
     </div>
     <div class="video">
         <video class="tw-w-full tw-rounded-lg" 
