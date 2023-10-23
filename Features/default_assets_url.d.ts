181419797ea16cc2a3c3dea13f32e39f3c250281
gparant <gregoire@workadventu.re>
2023-10-23 12:59:10 +0000

    Deploying to gh-pages from @ workadventure/scripting-api-extra@124d6f1b2d8939dfe33276617c0949ede9e94ca5 🚀

--- Features/default_assets_url.d.ts
+++ Features/default_assets_url.d.ts
@@ -1,1 +1,1 @@
-export declare const defaultAssetsUrl = "https://unpkg.com/@workadventure/scripting-api-extra@/dist";
+export declare const defaultAssetsUrl = "https://unpkg.com/@workadventure/scripting-api-extra@1.5.0/dist";
