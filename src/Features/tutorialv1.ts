b7a898e52b419a784510cc3b6258c00580ab2000
grégoire parant <g.parant@thecodingmachine.com>
2023-11-23 07:32:50 +0100

    fix: tutorial v1 luncher function (#228)
    The luncher function has `process.env` and it not works in some use case.

--- src/Features/tutorialv1.ts
+++ src/Features/tutorialv1.ts
@@ -1,26 +1,15 @@
 import { defaultAssetsUrl } from "./default_assets_url";
 
 export function launchTutorialv1(): void {
-    let host = defaultAssetsUrl;
-    if (
-        process.env.NODE_ENV === "development" &&
-        process.env.WORKADVENTURE_URL != undefined &&
-        process.env.WORKADVENTURE_URL !== ""
-    ) {
-        host = process.env.WORKADVENTURE_URL.replace("play.", "extra.");
-    }
-
-    const tutoUrl = `${host}/tutorialv1.html`;
+    const tutoUrl = `${defaultAssetsUrl}/tutorialv1.html`;
     console.info("Start onboarding application!", tutoUrl);
 
     console.info("Player tutorial done information: ", WA.player.state.tutorialDone);
     if (WA.player.state.tutorialDone) return;
 
     //open modal and show onboarding tuto
-    //TODO delete ts-ignore when new scripting release is up
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     WA.ui.modal.openModal({
+        tiltle: "Welcome onboard!",
         src: tutoUrl,
         allow: "fullscreen; clipboard-read; clipboard-write",
         allowApi: true,
