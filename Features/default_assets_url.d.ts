100afe9d73864e032c459fa126330c34d93a8259
moufmouf <d.negrier@thecodingmachine.com>
2025-01-09 12:58:27 +0000

    Deploying to gh-pages from @ workadventure/scripting-api-extra@97c881e54a953d4c1eb12900f06d71657f458c2b 🚀

--- Features/default_assets_url.d.ts
+++ Features/default_assets_url.d.ts
@@ -1,1 +1,1 @@
-export declare const defaultAssetsUrl = "https://unpkg.com/@workadventure/scripting-api-extra@1.9.3/dist";
+export declare const defaultAssetsUrl = "https://unpkg.com/@workadventure/scripting-api-extra@/dist";
