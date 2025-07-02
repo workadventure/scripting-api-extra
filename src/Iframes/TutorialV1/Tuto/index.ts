5a807b65277326bdc417a0e9c57086af9214789f
Kili S <79143155+klinshy@users.noreply.github.com>
2025-07-02 15:53:33 +0200

    added portuguese (brazil)

--- src/Iframes/TutorialV1/Tuto/index.ts
+++ src/Iframes/TutorialV1/Tuto/index.ts
@@ -6,6 +6,7 @@ import * as en from "../../../translate/en-US/index";
 import * as fr from "../../../translate/fr-FR/index";
 import * as ar from "../../../translate/ar-SA/index";
 import * as de from "../../../translate/de-DE/index";
+import * as pt from "../../../translate/pt-Br/index";
 
 i18next.init({
     debug: true,
@@ -23,6 +24,7 @@ i18next.init({
         de: {
             translation: de,
         },
+        pt: { translation: pt },
     },
 });
 
